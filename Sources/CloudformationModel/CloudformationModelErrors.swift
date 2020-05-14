b53f2bb7ecc02b3df0b6c49e0d4665b7ddff3f06
Simon Pilkington <simonmpilkington@icloud.com>
2020-05-13 18:57:17 -0700

    Add addition CloudFormation errors.

--- Sources/CloudformationModel/CloudformationModelErrors.swift
+++ Sources/CloudformationModel/CloudformationModelErrors.swift
@@ -32,50 +32,92 @@ public extension Swift.Error {
     }
 }
 
+private let accessDeniedIdentity = "AccessDeniedException"
 private let alreadyExistsIdentity = "AlreadyExistsException"
 private let cFNRegistryIdentity = "CFNRegistryException"
 private let changeSetNotFoundIdentity = "ChangeSetNotFound"
 private let createdButModifiedIdentity = "CreatedButModifiedException"
+private let incompleteSignatureIdentity = "IncompleteSignature"
 private let insufficientCapabilitiesIdentity = "InsufficientCapabilitiesException"
+private let internalFailureIdentity = "InternalFailure"
+private let invalidActionIdentity = "InvalidAction"
 private let invalidChangeSetStatusIdentity = "InvalidChangeSetStatus"
+private let invalidClientTokenIdIdentity = "InvalidClientTokenId"
 private let invalidOperationIdentity = "InvalidOperationException"
+private let invalidParameterCombinationIdentity = "InvalidParameterCombination"
+private let invalidParameterValueIdentity = "InvalidParameterValue"
+private let invalidQueryParameterIdentity = "InvalidQueryParameter"
 private let invalidStateTransitionIdentity = "InvalidStateTransition"
 private let limitExceededIdentity = "LimitExceededException"
+private let malformedQueryStringIdentity = "MalformedQueryString"
+private let missingActionIdentity = "MissingAction"
+private let missingAuthenticationTokenIdentity = "MissingAuthenticationToken"
+private let missingParameterIdentity = "MissingParameter"
 private let nameAlreadyExistsIdentity = "NameAlreadyExistsException"
 private let operationIdAlreadyExistsIdentity = "OperationIdAlreadyExistsException"
 private let operationInProgressIdentity = "OperationInProgressException"
 private let operationNotFoundIdentity = "OperationNotFoundException"
 private let operationStatusCheckFailedIdentity = "ConditionalCheckFailed"
+private let optInRequiredIdentity = "OptInRequired"
+private let requestExpiredIdentity = "RequestExpired"
+private let serviceUnavailableIdentity = "ServiceUnavailable"
 private let stackInstanceNotFoundIdentity = "StackInstanceNotFoundException"
 private let stackSetNotEmptyIdentity = "StackSetNotEmptyException"
 private let stackSetNotFoundIdentity = "StackSetNotFoundException"
 private let staleRequestIdentity = "StaleRequestException"
+private let throttlingIdentity = "ThrottlingException"
 private let tokenAlreadyExistsIdentity = "TokenAlreadyExistsException"
 private let typeNotFoundIdentity = "TypeNotFoundException"
-private let __accessDeniedIdentity = "AccessDenied"
+private let validationIdentity = "ValidationError"
+
+public struct CloudformationErrorPayload: Codable {
+    public let type: String
+    public let message: String
+
+    enum CodingKeys: String, CodingKey {
+        case type = "Code"
+        case message = "Message"
+    }
+}
 
 public enum CloudformationError: Swift.Error, Decodable {
+    case accessDenied(CloudformationErrorPayload)
     case alreadyExists(AlreadyExistsException)
     case cFNRegistry(CFNRegistryException)
     case changeSetNotFound(ChangeSetNotFoundException)
     case createdButModified(CreatedButModifiedException)
+    case incompleteSignature(CloudformationErrorPayload)
     case insufficientCapabilities(InsufficientCapabilitiesException)
+    case internalFailure(CloudformationErrorPayload)
+    case invalidAction(CloudformationErrorPayload)
     case invalidChangeSetStatus(InvalidChangeSetStatusException)
+    case invalidClientTokenId(CloudformationErrorPayload)
     case invalidOperation(InvalidOperationException)
+    case invalidParameterCombination(CloudformationErrorPayload)
+    case invalidParameterValue(CloudformationErrorPayload)
+    case invalidQueryParameter(CloudformationErrorPayload)
     case invalidStateTransition(InvalidStateTransitionException)
     case limitExceeded(LimitExceededException)
+    case malformedQueryString(CloudformationErrorPayload)
+    case missingAction(CloudformationErrorPayload)
+    case missingAuthenticationToken(CloudformationErrorPayload)
+    case missingParameter(CloudformationErrorPayload)
     case nameAlreadyExists(NameAlreadyExistsException)
     case operationIdAlreadyExists(OperationIdAlreadyExistsException)
     case operationInProgress(OperationInProgressException)
     case operationNotFound(OperationNotFoundException)
     case operationStatusCheckFailed(OperationStatusCheckFailedException)
+    case optInRequired(CloudformationErrorPayload)
+    case requestExpired(CloudformationErrorPayload)
+    case serviceUnavailable(CloudformationErrorPayload)
     case stackInstanceNotFound(StackInstanceNotFoundException)
     case stackSetNotEmpty(StackSetNotEmptyException)
     case stackSetNotFound(StackSetNotFoundException)
     case staleRequest(StaleRequestException)
+    case throttling(CloudformationErrorPayload)
     case tokenAlreadyExists(TokenAlreadyExistsException)
     case typeNotFound(TypeNotFoundException)
-    case accessDenied(message: String?)
+    case validation(CloudformationErrorPayload)
     case validationError(reason: String)
     case unrecognizedError(String, String?)
 
@@ -94,6 +136,9 @@ public enum CloudformationError: Swift.Error, Decodable {
         }
 
         switch errorReason {
+        case accessDeniedIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.accessDenied(errorPayload)
         case alreadyExistsIdentity:
             let errorPayload = try AlreadyExistsException(from: decoder)
             self = CloudformationError.alreadyExists(errorPayload)
@@ -106,21 +151,54 @@ public enum CloudformationError: Swift.Error, Decodable {
         case createdButModifiedIdentity:
             let errorPayload = try CreatedButModifiedException(from: decoder)
             self = CloudformationError.createdButModified(errorPayload)
+        case incompleteSignatureIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.incompleteSignature(errorPayload)
         case insufficientCapabilitiesIdentity:
             let errorPayload = try InsufficientCapabilitiesException(from: decoder)
             self = CloudformationError.insufficientCapabilities(errorPayload)
+        case internalFailureIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.internalFailure(errorPayload)
+        case invalidActionIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.invalidAction(errorPayload)
         case invalidChangeSetStatusIdentity:
             let errorPayload = try InvalidChangeSetStatusException(from: decoder)
             self = CloudformationError.invalidChangeSetStatus(errorPayload)
+        case invalidClientTokenIdIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.invalidClientTokenId(errorPayload)
         case invalidOperationIdentity:
             let errorPayload = try InvalidOperationException(from: decoder)
             self = CloudformationError.invalidOperation(errorPayload)
+        case invalidParameterCombinationIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.invalidParameterCombination(errorPayload)
+        case invalidParameterValueIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.invalidParameterValue(errorPayload)
+        case invalidQueryParameterIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.invalidQueryParameter(errorPayload)
         case invalidStateTransitionIdentity:
             let errorPayload = try InvalidStateTransitionException(from: decoder)
             self = CloudformationError.invalidStateTransition(errorPayload)
         case limitExceededIdentity:
             let errorPayload = try LimitExceededException(from: decoder)
             self = CloudformationError.limitExceeded(errorPayload)
+        case malformedQueryStringIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.malformedQueryString(errorPayload)
+        case missingActionIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.missingAction(errorPayload)
+        case missingAuthenticationTokenIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.missingAuthenticationToken(errorPayload)
+        case missingParameterIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.missingParameter(errorPayload)
         case nameAlreadyExistsIdentity:
             let errorPayload = try NameAlreadyExistsException(from: decoder)
             self = CloudformationError.nameAlreadyExists(errorPayload)
@@ -136,6 +214,15 @@ public enum CloudformationError: Swift.Error, Decodable {
         case operationStatusCheckFailedIdentity:
             let errorPayload = try OperationStatusCheckFailedException(from: decoder)
             self = CloudformationError.operationStatusCheckFailed(errorPayload)
+        case optInRequiredIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.optInRequired(errorPayload)
+        case requestExpiredIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.requestExpired(errorPayload)
+        case serviceUnavailableIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.serviceUnavailable(errorPayload)
         case stackInstanceNotFoundIdentity:
             let errorPayload = try StackInstanceNotFoundException(from: decoder)
             self = CloudformationError.stackInstanceNotFound(errorPayload)
@@ -148,14 +235,18 @@ public enum CloudformationError: Swift.Error, Decodable {
         case staleRequestIdentity:
             let errorPayload = try StaleRequestException(from: decoder)
             self = CloudformationError.staleRequest(errorPayload)
+        case throttlingIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.throttling(errorPayload)
         case tokenAlreadyExistsIdentity:
             let errorPayload = try TokenAlreadyExistsException(from: decoder)
             self = CloudformationError.tokenAlreadyExists(errorPayload)
         case typeNotFoundIdentity:
             let errorPayload = try TypeNotFoundException(from: decoder)
             self = CloudformationError.typeNotFound(errorPayload)
-        case __accessDeniedIdentity:
-            self = .accessDenied(message: errorMessage)
+        case validationIdentity:
+            let errorPayload = try CloudformationErrorPayload(from: decoder)
+            self = CloudformationError.validation(errorPayload)
         default:
             self = CloudformationError.unrecognizedError(errorReason, errorMessage)
         }
