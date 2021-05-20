60fe29c34ded1b5d74613caffc2734f6b34a2a38
Simon Pilkington <simonpi@amazon.com>
2021-05-20 14:10:37 -0700

    Align watchOS platform with dependency requirements.

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "smoke-aws",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13)
+        .macOS(.v10_15), .iOS(.v13), .watchOS(.v6), .tvOS(.v13)
         ],
     products: [
         .library(
