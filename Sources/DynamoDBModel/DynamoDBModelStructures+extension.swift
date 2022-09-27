0c47986ede0e2a448d7a09e63587f0c35885ce7d
Haotian Zhu <haotzhu@amazon.com>
2022-09-27 12:43:00 -0400

    add extension file

---
+++ Sources/DynamoDBModel/DynamoDBModelStructures+extension.swift
@@ -0,0 +1,28 @@
+// Copyright 2018-2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length line_length identifier_name type_name vertical_parameter_alignment
+// swiftlint:disable type_body_length function_body_length generic_type_name cyclomatic_complexity
+// -- Generated Code; do not edit --
+//
+// DynamoDBModelStructures+extension.swift
+// DynamoDBModel
+//
+
+extension BatchStatementError: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(self.code)
+        hasher.combine(self.message)
+    }
+}
